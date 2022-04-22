52b72df66d70a616cdabeef4b8b1af9c66290934
jonathanmanoel <jonathan.silva.manoel@gmail.com>
2022-04-22 14:57:45 -0300

    Ajustando a Data o formato alterando no settings.py

--- apissys/app/ssysemployee/models.py
+++ apissys/app/ssysemployee/models.py
@@ -1,14 +1,19 @@
+import time
+
 from django.db import models
 
 
 # Create your models here.
+from apissys import settings
+
+
 class Employees(models.Model):
     id = models.BigIntegerField(primary_key=True, editable=True)
     name = models.CharField(max_length=255, null=False, blank=False)
     email = models.EmailField(max_length=255, null=False, blank=False)
     department = models.CharField(max_length=255, null=False, blank=False)
     salary = models.IntegerField(auto_created=True, editable=True)
-    birth_date = models.DateField(auto_created=True, editable=True)
+    birth_date = models.DateField(editable=True)
 
     class Meta:
         verbose_name = u'Employees'
--- apissys/settings.py
+++ apissys/settings.py
@@ -16,7 +16,6 @@ from decouple import config, Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-
 
 # Application definition
 
@@ -74,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'apissys.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -84,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -116,7 +111,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -126,3 +120,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DATE_FORMAT': "%d/%m/%Y",
+    'DATE_INPUT_FORMATS': ["%d-%m-%Y"],
+}
