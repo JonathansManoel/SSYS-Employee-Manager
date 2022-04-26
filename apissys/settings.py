fb20e64ee7ba040da761ec72bb77cb71221532c4
jonathanmanoel <jonathan.silva.manoel@gmail.com>
2022-04-26 17:57:22 -0300

    conn_max_age

--- apissys/settings.py
+++ apissys/settings.py
@@ -14,10 +14,9 @@ from functools import partial
 from pathlib import Path
 import dj_database_url
 from decouple import config, Csv
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
