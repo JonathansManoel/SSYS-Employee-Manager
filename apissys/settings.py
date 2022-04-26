c6809a52349bad48cbdeee2161be7efe7e5314f0
jonathanmanoel <jonathan.silva.manoel@gmail.com>
2022-04-26 16:19:10 -0300

    Deploy no Heroku Finalizando

--- apissys/settings.py
+++ apissys/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'apissys.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 
-default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+default_db = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-parse_database = partial(dj_database_url.parse, conn_max_age=600)
+database_url = partial(dj_database_url.parse, conn_max_age=600)
 
 DATABASES = {
-    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
+    'default': config('DATABASE_URL', default=default_db, cast=database_url)
 }
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
